a00dbbd96478cfc543bc62df2b7f7da4f50e6f51
Naim Momin <mominnaim11@gmail.com>
2023-11-08 21:58:08 -0600

    The connection to the DB works, but need to display it on the HTML page

--- client/mainPage.js
+++ client/mainPage.js
@@ -1,3 +1,5 @@
+const http = require('http')
+const url = require('url')
 const sql = require("mssql");
 
 const dbConfig = {
@@ -8,12 +10,62 @@ const dbConfig = {
     port:1433
 
 };
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
 
+    if(req.method === 'GET' && parsedUrl.pathname === '/api/getArtists') {
+        sql.connect(dbConfig, (err) => {
+            if (err) {
+                console.error('Error connecting to the database:', err);
+                res.writeHead(500, { 'Content-Type': 'application/json'});
+                res.end(JSON.stringify({error: 'Database error'}));
+                return;
+            }
+
+            const request = new sql.Request();
+            request.query('SELECT * FROM [Artist]', (queryErr,result) => {
+                if (queryErr) {
+                    console.error('Error executing the query:', queryErr);
+                    res.writeHead(500, { 'Content-Type': 'application/json'});
+                    res.end(JSON.stringify({error: 'Database Query Error'}));
+                    sql.close();
+                    return;
+                }
+
+                res.writeHead(200, {'Content-type': 'text/plain' });
+                res.end(JSON.stringify(result.recordset));
+                sql.close();
+            });
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
+});
+
+//start the server
+const port = 3000;
+//const httpServer = server.listen(port, () => {
+//    console.log(`Server is running on port ${port}`);
+//  });
+
+const httpServer = server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  
+});
+
+httpServer.close(() => {
+    console.log(`Server on port ${port} is now closed`);
+  
+});
+  
+
+/*
 async function getUser() {
     let pool
     try {
         const pool = await sql.connect(dbConfig);
-        const result = await pool.request().query("SELECT * FROM [User]");
+        const result = await pool.request().query("SELECT * FROM [Artist]");
 
         console.log(result.recordset);
     } catch (err) {
@@ -26,3 +78,5 @@ async function getUser() {
 }
 
 getUser();
+*/
+
